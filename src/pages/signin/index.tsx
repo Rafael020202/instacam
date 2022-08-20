e32c2e4345ba915400ac7e5b2478d0919b6bf45c
Rafael Luiz <luizrafael391@gmail.com>
2022-08-20 10:34:57 -0300

    feat: implement login

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,27 +1,71 @@
 import type { NextPage } from 'next'
+import { useState, FormEvent } from 'react';
+import { CgSpinnerAlt } from 'react-icons/cg';
+
 import { Container, BgLogin, FormContainer, Form } from './styled';
 
 import Input from '../../components/Input';
+import { signIn } from '../../utils/authentication';
 
-const signIn: NextPage = () => {
+interface Login {
+  email: string
+  password: string
+}
+
+const SignIn: NextPage = () => {
+  const [data, setData] = useState<Login>({ email: '', password: '' });
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (event: FormEvent<HTMLInputElement>) => {
+    event.preventDefault();
+
+    const { name, value } = event.currentTarget;
+    const change = {
+      ...data,
+      [name]: value
+    } as Login;
+
+    setData(change);
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    setLoading(true);
+    
+    try {
+      await signIn(data.email, data.password);
+    } catch (error: any) {
+      console.error('error', error.message);
+    } finally {
+      setLoading(false);
+    }
+
+  }
+
   return (
     <Container>
         <BgLogin>
           <span>Hot & charm</span>
         </BgLogin>
         <FormContainer>
-          <Form>
+          <Form onSubmit={(e) => handleSubmit(e)}>
             <span>Login</span>
             <Input 
               label='E-mail'
               placeholder='E-mail'
-              
+              name='email'
+              onChange={(e) => handleChange(e)}
+              value={data.email}
             />
             
             <Input 
               label='Password'
               placeholder='Password'
               type='password'
+              name='password'
+              onChange={(e) => handleChange(e)}
+              value={data.password}
             />
 
             <section>
@@ -29,11 +73,11 @@ const signIn: NextPage = () => {
               <a>Sign up</a>
             </section>
 
-            <button>Log In</button>
+            <button>{loading ?<CgSpinnerAlt/> :'LOGIN'}</button>
           </Form>
         </FormContainer>  
     </Container>
   )
 }
 
-export default signIn;
+export default SignIn;
