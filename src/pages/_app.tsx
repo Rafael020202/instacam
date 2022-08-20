11765edbebb251f36d0798a0d91228f11dded4e6
Rafael Luiz <luizrafael391@gmail.com>
2022-08-20 10:34:14 -0300

    feat: add page title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import GlobalStyle from '../styles/global';
+import Head from 'next/head';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return ( 
     <>
+      <Head>
+        <title>Hot & Charm</title>
+      </Head>
       <GlobalStyle/>
       <Component {...pageProps} />
     </>
