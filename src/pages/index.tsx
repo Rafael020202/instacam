95ef6fc79c6e8a696e47999762e258617007e78b
Rafael Luiz <luizrafael391@gmail.com>
2022-08-21 12:53:34 -0300

    feat: add Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from 'next'
 
+import { Container } from '../styles/home';
+import Header from '../components/Header';
+
+
 const Home: NextPage = () => {
   return (
-    <>
-    </>
+    <Container>
+      <Header/>
+    </Container>
   )
 }
 
